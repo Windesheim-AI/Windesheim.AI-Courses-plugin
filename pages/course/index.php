00da879b2a1e6720adad98b79e7230ea7184503d
Tim Bentum <tim_bentum@live.nl>
2023-11-07 16:24:05 +0100

    refactor: :recycle: Refactor course editing pages

---
+++ pages/course/index.php
@@ -0,0 +1,5 @@
+<?php
+//include all the pages
+require_once WingAI_PLUGIN_DIR . 'pages/course/wingai-course-block-edit-page.php';
+require_once WingAI_PLUGIN_DIR . 'pages/course/wingai-course-edit-page.php';
+require_once WingAI_PLUGIN_DIR . 'pages/course/wingai-courses-page.php';
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,2 @@
 <?php
-//include all the pages
-require_once WingAI_PLUGIN_DIR . 'pages/wingai-course-block-edit-page.php';
-require_once WingAI_PLUGIN_DIR . 'pages/wingai-course-edit-page.php';
-require_once WingAI_PLUGIN_DIR . 'pages/wingai-courses-page.php';
+require_once WingAI_PLUGIN_DIR . 'pages/course/index.php';
