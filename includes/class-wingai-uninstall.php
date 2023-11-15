fb3fea490f910819f61d4fec9287185c1d05af96
Tim Bentum <tim_bentum@live.nl>
2023-11-15 10:13:56 +0100

    feat: :sparkles: Reorder table drop queries in WingAI_Uninstall class

--- includes/class-wingai-uninstall.php
+++ includes/class-wingai-uninstall.php
@@ -9,8 +9,8 @@ class WingAI_Uninstall
         $courses_table = $wpdb->prefix . 'WingAI_Courses';
         $stages_table = $wpdb->prefix . 'WingAI_Stages';
         $blocks_table = $wpdb->prefix . 'WingAI_Stage_Blocks';
-        $wpdb->query("DROP TABLE IF EXISTS $courses_table");
-        $wpdb->query("DROP TABLE IF EXISTS $stages_table");
         $wpdb->query("DROP TABLE IF EXISTS $blocks_table");
+        $wpdb->query("DROP TABLE IF EXISTS $stages_table");
+        $wpdb->query("DROP TABLE IF EXISTS $courses_table");
     }
 }
