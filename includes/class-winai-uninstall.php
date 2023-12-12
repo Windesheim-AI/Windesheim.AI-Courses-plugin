93a010b6173c46fac34a5c589b4404e2215791ef
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-12-12 13:18:57 +0100

    chore: :construction: Debug (11)

--- includes/class-winai-uninstall.php
+++ includes/class-winai-uninstall.php
@@ -9,6 +9,11 @@ class WinAI_Uninstall
         $courses_table = $wpdb->prefix . 'WinAI_Courses';
         $stages_table = $wpdb->prefix . 'WinAI_Stages';
         $blocks_table = $wpdb->prefix . 'WinAI_Stage_Blocks';
+
+        $wpdb->query("TRUNCATE TABLE IF EXISTS $blocks_table");
+        $wpdb->query("TRUNCATE TABLE IF EXISTS $stages_table");
+        $wpdb->query("TRUNCATE TABLE IF EXISTS $courses_table");
+
         $wpdb->query("DROP TABLE IF EXISTS $blocks_table");
         $wpdb->query("DROP TABLE IF EXISTS $stages_table");
         $wpdb->query("DROP TABLE IF EXISTS $courses_table");
